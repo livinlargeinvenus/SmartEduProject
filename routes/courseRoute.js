f90e711087ff90dda53fb7462bc29cedc06c3035
Onur Alp <onuralpkayag@gmail.com>
2023-01-18 23:12:20 +0300

    creatte a course, course-teacher relationship

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const courseController = require('../controllers/courseController');
+const roleMiddlewware = require('../middlewares/roleMiddleware');
 
 const router = express.Router();
 
-router.route('/').post(courseController.createCourse); // http://localhost:3000/courses
+router
+    .route('/')
+    .post(roleMiddlewware(['teacher', 'admin']), courseController.createCourse); // http://localhost:3000/courses
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
 
