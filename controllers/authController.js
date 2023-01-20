ca48da110ac01c26b190ec89fcfa69cd92dfaaa5
Onur Alp <onuralpkayag@gmail.com>
2023-01-20 22:49:29 +0300

    course-teacher-student relationship

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 
 const User = require('../models/User');
 const Category = require('../models/Category');
+const Course = require('../models/Course');
 
 exports.createUser = async (req, res) => {
     try {
@@ -24,11 +25,9 @@ exports.loginUser = async (req, res) => {
 
         if (user) {
             bcrypt.compare(password, user.password, (error, same) => {
-                if (same) {
-                    // user session
-                    req.session.userID = user._id;
-                    res.status(200).redirect('/users/dashboard');
-                }
+                // user session
+                req.session.userID = user._id;
+                res.status(200).redirect('/users/dashboard');
             });
         }
     } catch (error) {
@@ -46,11 +45,15 @@ exports.logoutUser = (req, res) => {
 };
 
 exports.getDashboardPage = async (req, res) => {
-    const user = await User.findOne({ _id: req.session.userID });
+    const user = await User.findOne({ _id: req.session.userID }).populate(
+        'courses'
+    );
     const categories = await Category.find();
+    const courses = await Course.find({ user: req.session.userID });
     res.status(200).render('dashboard', {
         page_name: 'dashboard',
         user,
         categories,
+        courses,
     });
 };
